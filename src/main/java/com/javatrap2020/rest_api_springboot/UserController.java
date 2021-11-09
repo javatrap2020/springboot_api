b62e9b795447c52fb1cd844da01399f235a95385
Oleh Boiko <javatrap2020@gmail.com>
2021-11-09 18:56:41 +0200

    003

--- src/main/java/com/javatrap2020/rest_api_springboot/UserController.java
+++ src/main/java/com/javatrap2020/rest_api_springboot/UserController.java
@@ -1,4 +1,58 @@
 package com.javatrap2020.rest_api_springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.source.InvalidConfigurationPropertyValueException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId)
+        throws InvalidConfigurationPropertyValueException {
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new InvalidConfigurationPropertyValueException("User not found on :: ", userId,
+                "The specified resource does not exist."));
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@Validated @RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable(value = "id") Long userId, @Validated @RequestBody User userDetails)
+            throws InvalidConfigurationPropertyValueException {
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new InvalidConfigurationPropertyValueException("User not found on :: ", userId,
+                        "The specified resource does not exist."));
+        user.setEmail(userDetails.getEmail());
+        user.setLastName(userDetails.getLastName());
+        user.setFirstName(userDetails.getFirstName());
+        user.getUpdatedAt(new Date());
+    }
 }
