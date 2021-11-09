42a3bc478426b17616d732731d804757ed21b569
Oleh Boiko <javatrap2020@gmail.com>
2021-11-09 14:14:41 +0200

    springboot_api

---
+++ src/test/java/com/javatrap2020/rest_api_springboot/RestApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatrap2020.rest_api_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
